fecba02411414dc53f27f5718acac6dadb91e968
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-08-10 14:35:09 +0300

    added source selection for webcam demo

--- scripts/webcam_demo.py
+++ scripts/webcam_demo.py
@@ -39,16 +39,19 @@ def rescale(ori_shape, boxes, target_shape):
 
 class YoloV7:
     def __init__(self, args):
+        self.__source = args.source
         self.__conf_thres = args.conf_thres
         self.__iou_thres = args.iou_thres
         self.__img_size = args.img_size
         self.__device = args.device
         self.__visualize = args.visualize
+        self.__show_perf = args.show_perf
         self.__weights = os.path.join(YOLOV7_ROOT, args.weights)
         self.__model = attempt_load(self.__weights, map_location=self.__device)
         self.__names = self.__model.names
         self.__colors = [[random.randint(0, 255)
                           for _ in range(3)] for _ in self.__names]
+        self.model_info()
 
     @torch.no_grad()
     def inference(self, img: torch.Tensor):
@@ -65,6 +68,42 @@ class YoloV7:
         if detections:
             detections = detections[0]
         return detections
+
+    def model_info(self, verbose=False):
+        # Model information. img_size may be int or list, i.e. img_size=640 or img_size=[640, 320]
+        n_p = sum(x.numel()
+                  for x in self.__model.parameters())  # number parameters
+        n_g = sum(x.numel() for x in self.__model.parameters()
+                  if x.requires_grad)  # number gradients
+        if verbose:
+            rospy.loginfo('%5s %40s %9s %12s %20s %10s %10s' % ('layer', 'name',
+                                                                'gradient', 'parameters', 'shape', 'mu', 'sigma'))
+            for i, (name, p) in enumerate(self.__model.named_parameters()):
+                name = name.replace('module_list.', '')
+                rospy.loginfo('%5g %40s %9s %12g %20s %10.3g %10.3g' %
+                              (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
+
+        try:  # FLOPS
+            from thop import profile
+            stride = max(int(self.__model.stride.max()), 32) if hasattr(
+                self.__model, 'stride') else 32
+            img = torch.zeros((1, self.__model.yaml.get('ch', 3), stride, stride), device=next(
+                self.__model.parameters()).device)  # input
+            flops = profile(deepcopy(self.__model), inputs=(img,), verbose=False)[
+                0] / 1E9 * 2  # stride GFLOPS
+            img_size = img_size if isinstance(img_size, list) else [
+                img_size, img_size]  # expand if int/float
+            fs = ', %.1f GFLOPS' % (
+                flops * img_size[0] / stride * img_size[1] / stride)  # 640x640 GFLOPS
+        except (ImportError, Exception):
+            fs = ''
+        summary = f"\N{rocket}\N{rocket}\N{rocket} Yolov7 Detector summary:\n" \
+            + f"Weights: {self.__weights}\n" \
+            + f"{len(list(self.__model.modules()))} layers, {n_p} parameters, {n_g} gradients{fs}\n" \
+            + f"Confidence Threshold: {self.__conf_thres}\n" \
+            + f"IOU Threshold: {self.__iou_thres}\n" \
+            + f"Device: {self.__device}\n" 
+        logging.info(summary)
 
     def process_img(self, img_input):
         # automatically resize the image to the next smaller possible size
@@ -84,7 +123,7 @@ class YoloV7:
         return img
 
     def run(self):
-        cam = cv2.VideoCapture(0)
+        cam = cv2.VideoCapture(self.__source)
         while True:
             ret, frame = cam.read()
             if ret:
@@ -98,9 +137,10 @@ class YoloV7:
                     [h_scaled, w_scaled], detections[:, :4], [h_ori, w_ori])
                 detections[:, :4] = detections[:, :4].round()
                 end = time.perf_counter()
-                elapsed = end - start
-                logging.info(f"Inference time: {elapsed*1000:.2f} ms")
-                logging.info(f"FPS: {1/elapsed:.2f} ms")
+                if self.__show_perf:
+                    elapsed = end - start
+                    logging.info(f"Inference time: {elapsed*1000:.2f} ms")
+                    logging.info(f"FPS: {1/elapsed:.2f} ms")
 
                 if self.__visualize:
                     bboxes = [[int(x1), int(y1), int(x2), int(y2)]
@@ -118,15 +158,16 @@ def main(args):
     detector.run()
 
 if __name__ == "__main__":
-    logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights',  type=str, default='weights/yolov7-tiny.pt', help='model.pt path(s)')
+    parser.add_argument('--source',  type=str, default='/dev/video0', help='model.pt path(s)')
     parser.add_argument('--conf_thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou_thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--img_size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--visualize', action='store_true', default=True, help='existing project/name ok, do not increment')
-    parser.add_argument('--device', default='cuda', help='cuda or cpu')
+    parser.add_argument('--visualize', action='store_true', help='Show detection image')
+    parser.add_argument('--device', type=str, default='cuda', help='cuda or cpu')
+    parser.add_argument('--show_perf', action='store_true',help='log fps and inference time')
     args = parser.parse_args()
-    logging.info(args)
 
+    logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.INFO)
     main(args)
