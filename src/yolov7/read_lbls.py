8b04b4777b1e9b4a7c40b31b1addf92c816c0681
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-11-09 12:00:42 +0200

    updated read_lbls: choose lowest conf frames for each class

--- src/yolov7/read_lbls.py
+++ src/yolov7/read_lbls.py
@@ -1,6 +1,7 @@
 import os
 import os.path as osp
 import argparse
+import logging
 
 
 def get_label_dict(labels, dir):
@@ -21,16 +22,81 @@ def get_label_dict(labels, dir):
     return preds_dict
 
 
+def accumulate_predictions(preds_dict):
+    preds_accumulate = preds_dict
+    for frame, pred in preds_dict.items():
+        for cls in pred.keys():
+            all_preds = preds_accumulate[frame][cls]
+            if len(all_preds) != 0:
+                preds_accumulate[frame][cls] = sum(all_preds) / len(all_preds)
+            else:
+                preds_accumulate[frame][cls] = 0
+    return preds_accumulate
+
+
+def search_frame(preds_accumulate, lbl, conf, ext):
+    for frame, pred in preds_accumulate.items():
+        if pred[lbl] == conf:
+            logging.debug(f"Return frame {frame}, class: {lbl}, conf: {conf}")
+            return frame + ext
+    logging.error(f"Frame not found")
+
+
+def choose_lowest_conf_frames(preds_accumulate, num_of_imgs, labels, ext):
+    preds_by_class = {}
+    for _, pred in preds_accumulate.items():
+        for cls, conf in pred.items():
+            if conf != 0:
+                if cls not in preds_by_class.keys():
+                    preds_by_class[cls] = []
+                preds_by_class[cls].append(conf)
+    top_frames_list = []
+    for cls in labels:
+        preds_by_class[cls].sort()
+    for cls in labels:
+        img_taken = 0
+        pop_idx = 0
+        while img_taken < num_of_imgs:
+            top_conf = preds_by_class[cls].pop(pop_idx)
+            top_frame = search_frame(preds_accumulate, cls, top_conf, ext)
+            pop_idx += 1
+            if top_frame not in top_frames_list:
+                top_frames_list.append(top_frame)
+                img_taken += 1
+    return top_frames_list
+
+
 def main(opts):
     labels = list(range(opts.classes))
     preds_dict = get_label_dict(labels=labels, dir=opts.dir)
-    print(preds_dict)
+    preds_accumulate = accumulate_predictions(preds_dict=preds_dict)
+    top_frames_list = choose_lowest_conf_frames(
+        preds_accumulate=preds_accumulate,
+        num_of_imgs=opts.num_of_imgs,
+        labels=labels,
+        ext=opts.ext,
+    )
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="[%(levelname)s]: %(message)s", level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-d", "--dir", required=True, help="Directory with detections to read"
+    )
+    parser.add_argument(
+        "-x",
+        "--ext",
+        type=str,
+        default=".png",
+        help="Image file extension",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-of-imgs",
+        type=int,
+        default=10,
+        help="Num of images to label",
     )
     parser.add_argument(
         "-c",
