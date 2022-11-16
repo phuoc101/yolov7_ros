2ff953b45a0461648395fbc3ba9aeee097a7b2b4
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-11-17 00:59:39 +0200

    added initiation specifically for picam

--- src/yolov7/al_scripts/al_init_selection_drone.py
+++ src/yolov7/al_scripts/al_init_selection_drone.py
@@ -1,8 +1,10 @@
 from loguru import logger
+import shutil
 import os
 import sys
 import argparse
 import math
+import random
 from glob import glob
 
 
@@ -11,44 +13,36 @@ DATASETS_DIR = os.environ["DATASETS_CV_DIR"]
 DATASET_NAME = "picam_data/Full_train_test_val_5percent"
 
 
-def choose_images(files, percent, num_of_imgs):
-    files_total = len(files)
-    if percent > 0:
-        logger.debug(
-            f"Choosing {percent}% of images in set, which is {files_total * percent / 100} imgs"
-        )
-        percent /= 100
-        step = round(1 / percent)
-        return files[0:files_total:step], files[1:files_total:step]
-    elif num_of_imgs > 0:
-        logger.debug(f"Choosing {num_of_imgs} images in set")
-        step = files_total // num_of_imgs
-        return (
-            files[0 : num_of_imgs * step : step],
-            files[1 : num_of_imgs * step + 1 : step],
-        )
-    else:
-        logger.error("Need to choose percentage or num of imgs to take")
-        sys.exit(1)
+def choose_images(files, num_chosen, chosen):
+    files_to_choose = [f for f in files if f not in chosen]
+    chosen_train = random.sample(files_to_choose, num_chosen)
+    chosen += chosen_train
+
+    files_to_choose = [f for f in files if f not in chosen]
+    chosen_val = random.sample(files_to_choose, num_chosen)
+    chosen += chosen_val
+    return chosen_train, chosen_val
 
 
 def move_to_train_val(chosen_train, train_dir, chosen_val, val_dir):
     for file in chosen_train:
         basename = os.path.basename(file)
         new_file = os.path.join(train_dir, basename)
-        # os.replace(file, new_file)
+        shutil.move(file, new_file)
         logger.info(f"{file} -> {new_file}")
     for file in chosen_val:
         basename = os.path.basename(file)
         new_file = os.path.join(val_dir, basename)
-        # os.replace(file, new_file)
+        shutil.move(file, new_file)
         logger.info(f"{file} -> {new_file}")
 
 
 def main(opts):
-    train_dir = os.path.join(opts.dir, "train/images")
-    val_dir = os.path.join(opts.dir, "val/images")
-    test_dir = os.path.join(opts.dir, "test/images")
+    dir, sets, ext = opts.dir, opts.sets, opts.extension
+    percent = opts.percentage
+    train_dir = os.path.join(dir, "train/images")
+    val_dir = os.path.join(dir, "val/images")
+    test_dir = os.path.join(dir, "test/images")
     os.makedirs(train_dir, exist_ok=True)
     os.makedirs(val_dir, exist_ok=True)
     os.makedirs(test_dir, exist_ok=True)
@@ -61,30 +55,25 @@ def main(opts):
         logger.info(
             "Both training and validation directories are empty, start initializing"
         )
-    sets = opts.sets
-    ext = opts.extension
-    percent = opts.percentage
-    num_of_imgs = opts.num_of_imgs
-    chosen_imgs = []
+    chosen = []
+    all_files = os.listdir(test_dir)
+    num_of_files = len(all_files)
+    num_chosen = math.floor((percent / 100) * num_of_files / len(sets))
+    logger.debug(f"Choosing {num_chosen} images from each set")
     for set in sets:
-        files = glob(test_dir + f"/{set}*{ext}")
+        files = glob(test_dir + f"/*{set}*{ext}")
         logger.info(f"Found {len(files)} images in {set}")
         chosen_train, chosen_val = choose_images(
-            files=files, percent=percent, num_of_imgs=num_of_imgs
+            files=files, num_chosen=num_chosen, chosen=chosen
         )
         logger.debug(f"Chosen for training: {chosen_train}")
         logger.debug(f"Chosen for validation: {chosen_val}")
-        move_to_train_val(
-            chosen_train=chosen_train,
-            train_dir=train_dir,
-            chosen_val=chosen_val,
-            val_dir=val_dir,
-        )
+        move_to_train_val(chosen_train, train_dir, chosen_val, val_dir)
 
 
 if __name__ == "__main__":
     logger.remove()
-    logger.add(sys.stderr, level="INFO")
+    logger.add(sys.stderr, level="DEBUG")
     parser = argparse.ArgumentParser()
     # Dataset should initially have train, test, val dirs, all images in test directory
     parser.add_argument(
@@ -99,13 +88,7 @@ if __name__ == "__main__":
         "-s",
         "--sets",
         nargs="+",
-        default=[
-            "Hevolinna1_no_drone",
-            "Hevolinna1_drone",
-            "Hevolinna2_no_drone",
-            "Hevolinna2_drone",
-            "Narnia",
-        ],
+        default=["no_drone", "has_drone", "has_close_person", "has_close_drone"],
         help="List of datasets",
     )
     parser.add_argument(
@@ -120,14 +103,7 @@ if __name__ == "__main__":
         "--percentage",
         type=float,
         default=0,
-        help="Percentage of image to take from each set (use either this or number of images, will prioritize this if both are selected)",
-    )
-    parser.add_argument(
-        "-n",
-        "--num-of-imgs",
-        type=int,
-        default=0,
-        help="Number of images to take from each set (use either this or percentage)",
+        help="Percentage of image to take from dataset (use either this or number of images, will prioritize this if both are selected)",
     )
     opts = parser.parse_args()
     main(opts=opts)
